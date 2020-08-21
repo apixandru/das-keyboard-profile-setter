7a8c10f08e8c05c652f3ab0159fb2b93966335e8
apixandru <contact@apixandru.com>
2020-08-21 09:00:23 -0700

    add more profiles

--- src/main/java/com/apixandru/keyboard/Main.java
+++ src/main/java/com/apixandru/keyboard/Main.java
@@ -1,81 +1,74 @@
 package com.apixandru.keyboard;
 
-import com.apixandru.keyboard.sequences.CyanSequences;
-import com.apixandru.keyboard.sequences.GreenSequences;
-import com.apixandru.keyboard.sequences.OrangeSequences;
-import com.apixandru.keyboard.sequences.White1Sequences;
 import org.hid4java.HidDevice;
 import org.hid4java.HidManager;
 import org.hid4java.HidServices;
 
-import java.lang.reflect.Field;
-import java.util.ArrayList;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
+
+import static com.apixandru.keyboard.Utils.parseByteArrays;
 
 public class Main {
 
-    public static void main(String[] args) throws InterruptedException {
+    private static final List<String> YES_ANSWERS = Arrays.asList("Y", "y", "");
+    private static final List<String> NO_ANSWERS = Arrays.asList("n", "N", "NO", "no");
+
+    public static void main(String[] args) throws InterruptedException, URISyntaxException, IOException {
         HidServices hidServices = HidManager.getHidServices();
         List<HidDevice> attachedHidDevices = hidServices.getAttachedHidDevices();
         for (HidDevice attachedHidDevice : attachedHidDevices) {
             if (attachedHidDevice.getProductId() == 0x202B && attachedHidDevice.getInterfaceNumber() == 2) {
-                System.out.println(attachedHidDevice);
                 attachedHidDevice.open();
 
-                try {
+                URI uri = Main.class
+                        .getResource("/profiles")
+                        .toURI();
+                Path profiles = Paths.get(uri);
+                List<Path> collect = Files.list(profiles)
+                        .collect(Collectors.toList());
 
-                    List<Class> colors = Arrays.asList(
-                            OrangeSequences.class,
-                            GreenSequences.class,
-                            White1Sequences.class,
-                            CyanSequences.class);
-
-                    for (Class color : colors) {
-                        for (int[] sequence : getFields(color)) {
-                            int write = writeMessage(attachedHidDevice, asBytes(sequence));
-                            System.out.println(write);
-                            Thread.sleep(50);
+                BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+                for (Path profile : collect) {
+                    while (true) {
+                        System.out.print("Continue with " + profile.getFileName() + " ? ");
+                        String s = br.readLine();
+                        if (NO_ANSWERS.contains(s)) {
+                            return;
+                        } else if (YES_ANSWERS.contains(s)) {
+                            break;
                         }
                     }
-                } finally {
-                    attachedHidDevice.close();
+                    List<byte[]> strings = readBytes(profile);
+                    for (byte[] sequence : strings) {
+                        int write = writeMessage(attachedHidDevice, sequence);
+                        if (write != 65) {
+                            System.err.println("Expected 65 but got " + write + ". I have no idea what this means.");
+                        }
+                        Thread.sleep(50);
+                    }
                 }
-
             }
         }
+    }
+
+    private static List<byte[]> readBytes(Path profile) throws IOException {
+        List<String> strings = Files.readAllLines(profile);
+        strings.removeIf(next -> next.endsWith("."));
+        return parseByteArrays(strings);
     }
 
     private static int writeMessage(HidDevice attachedHidDevice, byte[] message) {
         return attachedHidDevice.write(message, message.length, (byte) 0);
     }
 
-    private static byte[] asBytes(int... vals) {
-        int[] ints = vals;
-        if (ints.length > 64) {
-            ints = Arrays.copyOfRange(vals, vals.length - 64, vals.length);
-        }
-
-        byte[] bytes = new byte[64];
-        for (int i = 0; i < ints.length; i++) {
-            bytes[i] = (byte) ints[i];
-        }
-        return bytes;
-    }
-
-    public static List<int[]> getFields(Class<?> sequenceClass) {
-        try {
-            Field[] declaredFields = sequenceClass.getDeclaredFields();
-            if (declaredFields.length != 25) {
-                throw new IllegalArgumentException();
-            }
-            List<int[]> values = new ArrayList<>();
-            for (Field field : declaredFields) {
-                values.add((int[]) field.get(null));
-            }
-            return values;
-        } catch (IllegalAccessException ex) {
-            throw new IllegalStateException(ex);
-        }
-    }
 }
---
+++ src/main/java/com/apixandru/keyboard/Utils.java
@@ -0,0 +1,48 @@
+package com.apixandru.keyboard;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Utils {
+
+    public static byte hexStringToByteArray(String hexByteString) {
+        if (hexByteString == null || !hexByteString.startsWith("0x")) {
+            throw new IllegalArgumentException(hexByteString);
+        }
+        String hexWithout0x = hexByteString.substring(2);
+        byte parsedValue = (byte) Integer.parseInt(hexWithout0x, 16);
+        String backToHex = String.format("%02x", parsedValue);
+        if (!hexWithout0x.equals(backToHex)) {
+            throw new IllegalArgumentException("Failed to convert " + hexByteString);
+        }
+        return parsedValue;
+    }
+
+    public static byte[] parseHexBytes(List<String> hexLines) {
+        String[] hexStrings = hexLines.stream()
+                .map(line -> line.split(" "))
+                .flatMap(Arrays::stream)
+                .toArray(String[]::new);
+
+        byte[] bytes = new byte[hexStrings.length];
+        for (int i = 0; i < hexStrings.length; i++) {
+            bytes[i] = hexStringToByteArray(hexStrings[i]);
+        }
+        return bytes;
+    }
+
+    public static List<byte[]> parseByteArrays(List<String> strings) {
+        List<byte[]> packages = new ArrayList<>();
+        List<String> buffer = new ArrayList<>();
+        for (String string : strings) {
+            if (string.isEmpty()) {
+                packages.add(parseHexBytes(buffer));
+                buffer = new ArrayList<>();
+            } else {
+                buffer.add(string);
+            }
+        }
+        return packages;
+    }
+}
